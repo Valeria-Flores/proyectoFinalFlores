e5554e6723c9a82342c94472a697d23c162bfdfb
valefpz <valeriaflores.pz@gmail.com>
2023-07-26 23:52:08 -0600

    peticion

--- main.js
+++ main.js
@@ -11,16 +11,17 @@ class Producto{
 
 }
 
-/* Instanciar catalogo */
-const prod1 = new Producto(1,"Mesa de comedor", "Mesa de comedor de elegante y robusta, con un acabado en madera que le brinda calidez y versatilidad para adaptarse a cualquier estilo de decoración.",3000,"mesacomedor.webp",1);
-const prod2 = new Producto(2,"Escritorio","El escritorio blanco presenta líneas limpias y modernas, con amplio espacio de trabajo, compartimentos de almacenamiento integrados y una superficie resistente, perfecto para crear un ambiente funcional y organizado en cualquier oficina o estudio.",1500,"escritorio.webp",1);
-const prod3 = new Producto(3,"Cama individual","La base de cama individual ofrece un diseño contemporáneo y elegante, con una estructura sólida y resistente que brinda soporte óptimo, mientras que su tono neutro se adapta fácilmente a cualquier estilo de dormitorio.",3000,"camaind.webp",1);
-const prod4 = new Producto(4,"Cama matrimonial","La base de cama matrimonial combina funcionalidad y estilo, con un diseño moderno, una estructura robusta y un tono suave que aporta luminosidad y tranquilidad al espacio de descanso.",5000,"camamat.webp",1);
-const prod5 = new Producto(5,"Set de sillas","El set de 4 sillas blancas para comedor combina un diseño moderno y minimalista con la comodidad y durabilidad de su estructura ergonómica, brindando un toque de elegancia y luminosidad a cualquier espacio.",3000,"silla.webp",1);
-const prod6 = new Producto(6,"Set de bancos","El set de 4 bancos cuenta con un diseño moderno y minimalista, asientos cómodos y una estructura duradera, ofreciendo una solución versátil y elegante para completar el mobiliario de cualquier bar o cocina.",2250,"banco.webp",1);
-const prod7 = new Producto(7,"Librero","El librero blanco presenta un diseño contemporáneo y funcional, con estantes ajustables para adaptarse a diferentes tamaños de libros y objetos decorativos, brindando organización y estilo a cualquier espacio de almacenamiento.",4800,"librero.webp",1);
-const prod8 = new Producto(8,"Buró","El buró combina belleza y funcionalidad con su diseño elegante y acabado en madera, ofreciendo espacio de almacenamiento con cajones espaciosos y una superficie resistente, ideal para complementar cualquier dormitorio con estilo y practicidad.",1600,"buro.webp",1);
-const prod9 = new Producto(9,"Armario","El armario blanco presenta un diseño clásico y atemporal, con amplio espacio de almacenamiento, estantes ajustables y un elegante acabado en blanco que se adapta a cualquier estilo de decoración, proporcionando organización y estilo a cualquier habitación.",5000,"armario.webp",1);
+/* Petición */
+const cargarCatalogo = async () =>{
+   const res = await fetch("muebles.json");
+   const data = await res.json();
+   for(let mueble of data){
+      let nProducto = new Producto(mueble.id, mueble.nombre, mueble.descripcion, mueble.precio, mueble.imagen, mueble.cantidad);
+      productos.push(nProducto);
+   }
+   console.log(productos);
+   localStorage.setItem("productos", JSON.stringify(productos));
+}
 
 /* Array del catalogo */
 let productos = []; 
@@ -29,8 +30,7 @@ let productos = [];
 if(localStorage.getItem("productos")){
     productos = JSON.parse(localStorage.getItem("productos"));
 }else{
-    productos.push(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9);
-    localStorage.setItem("productos", JSON.stringify(productos));
+    cargarCatalogo();
 }
 console.log(productos);
 
@@ -165,3 +165,5 @@ function añadirCarrito(producto){
        break
     }
  });
+
+
